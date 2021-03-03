2ab10947fe77617adaa72387a2a5004bfa18c932
Luis Mazariego <mazariego2011@gmail.com>
2021-03-02 20:54:15 -0600

    xtx

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CountriesModule } from './countries/countries.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'countries',
+    loadChildren: () =>
+      import('./countries/countries.module').then((m) => m.CountriesModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'countries',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent implements OnInit {
+
+  myForm: FormGroup = this.fb.group({
+    region: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  guardar() {
+    console.log(this.myForm.value);
+    
+  }
+
+}
